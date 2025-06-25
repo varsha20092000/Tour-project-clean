3a046cf1acb6f0f780c7c0cd8277b05340258eb5
Varsha <varsha20092000@gmail.com>
2025-06-25 19:54:41 +0530

    Add STATIC_ROOT for deployment

--- journy/settings.py
+++ journy/settings.py
@@ -156,10 +156,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'App', 'static')]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'journy', 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'App', 'static'),
+    os.path.join(BASE_DIR, 'journy', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
