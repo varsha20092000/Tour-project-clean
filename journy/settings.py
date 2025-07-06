81abbc239e4128f36a8b425bc162c2e14ace0e2f
Varsha <varsha20092000@gmail.com>
2025-07-06 13:59:57 +0530

    Add .gitattributes to normalize line endings

--- journy/settings.py
+++ journy/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, 'static'),
+   
     os.path.join(BASE_DIR, 'App', 'static'),
     os.path.join(BASE_DIR, 'journy', 'static'),
 ]
