2c5331d5b54fe7013278e2bfd6a3fcd8c1e3fea7
Varsha <varsha20092000@gmail.com>
2025-07-06 11:10:42 +0530

    Fix static file configuration in settings.py

--- journy/settings.py
+++ journy/settings.py
@@ -30,8 +30,7 @@ import os
 
 STATIC_URL = '/static/'
 
-# Add this line:
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -163,6 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'App', 'static'),
     os.path.join(BASE_DIR, 'journy', 'static'),
 ]
